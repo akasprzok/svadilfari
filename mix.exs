8638c5b06c1e84545790c37b48a99e6b036b3a9b
Andreas Kasprzok <akasprzok@gmail.com>
2022-06-27 22:53:00 -0700

    License

--- mix.exs
+++ mix.exs
@@ -1,13 +1,31 @@
 defmodule Svadilfari.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+@url "https://github.com/akasprzok/svadilfari"
+
   def project do
     [
       app: :svadilfari,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: description(),
+      package: package(),
+      source_url: @url,
+      docs: docs(),
+
+      # Testing
+      test_coverage: [tool: ExCoveralls],
+      preferred_clie_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ]
     ]
   end
 
@@ -21,8 +39,32 @@ defmodule Svadilfari.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:sleipnir, "~> 0.1"}
+    ]
+  end
+
+  defp description do
+    """
+    A Logger Backend for sending logs directly to Grafana Loki.
+    """
+  end
+
+  defp package do
+    [
+      licneses: ["MIT"],
+      links: %{"GitHub" => @url},
+      files: ~w(mix.exs lib README.md LICENSE.md),
+      maintainers: ["Andreas Kasprzok"]
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Svadilfari",
+      extras: ["README.md"]
     ]
   end
 end
